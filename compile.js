e7229346dfea9ede354c9a4b187364e68218e12b
Luiserebii <luis@serebii.io>
2019-03-07 02:10:23 -0500

    Writing compile function helper in generating the compilation input, still debugging

--- compile.js
+++ compile.js
@@ -5,8 +5,8 @@ const solc = require('solc');
 
 const contracts = {
 
-   'MyResolver.sol': 'contracts',
-   'SnowflakeResolver.sol': 'contracts',
+   'MyResolver.sol': [ 'contracts' ],
+   'SnowflakeResolver.sol': [ 'contracts' ],
 
    'Ownable.sol': [ 'contracts', 'zeppelin', 'ownership' ],
 
@@ -16,21 +16,38 @@ const contracts = {
 
 }
 
+//Maybe move this to some sort of personal util file?
+function generateContractSources(contractInput) {
+   
+   let sources = {};
+   
+   for(var name in contractInput){
+      
+      let pathArguments = contractInput[name];
+      //These functions are unfortunately not-chainable
+      pathArguments.push(name);
+      pathArguments.splice(0, 0, __dirname);
 
+      //Pass path.resolve params via array by apply()
+      let contractPath = path.resolve.apply(null, pathArguments);
+      console.log(contractPath);
+      let contractSource = fs.readFileSync(contractPath, 'utf8');
+      
+      //Finally, add it to our sources object!
+      sources[name] = contractSource;
+      
+   }
+   return sources;
 
-const calcPath = path.resolve(__dirname, 'contracts', 'Calculator.sol');
+}
 
 
 const input = {
-
-   sources: {
-      'Calculator.sol': fs.readFileSync(calcPath, 'utf8')
-   }
-
+   sources: generateContractSources(contracts)
 };
-
+console.log(generateContractSources(contracts));
 
 compileOutput = solc.compile(input, 1);
-//console.log(compileOutput) 
+console.log(compileOutput);
 
 module.exports = compileOutput.contracts;
