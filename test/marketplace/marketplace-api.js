4e8c0a151e4b4e26c536d8a6952d289d44dd53d2
Luiserebii <luis@serebii.io>
2019-03-29 18:56:00 -0400

    Finished test-data.js, moved over helper functions from main test file into marketplace-api.js

--- test/marketplace/marketplace-api.js
+++ test/marketplace/marketplace-api.js
@@ -17,6 +17,55 @@ const enums = allEnums.CouponMarketPlaceResolverInterface;
 class MarketplaceAPI {
 
 
+  async function assertSolidityRevert(run, expectedErr = null){
+    let err;
+    try {
+      await run();
+    } catch(_e) {
+      err = _e.message;
+    }
+    assert.isTrue(err.includes('VM Exception while processing transaction: revert'));
+    if(expectedErr != null) assert.isTrue(err.includes(expectedErr));
+
+    return err;
+  }
+
+  
+  //Convenience function, assumes instances is set with loaded contracts
+  async function addToIdentityRegistrySimple(userIdentity) {
+    await addToIdentityRegistry(userIdentity, instances.IdentityRegistry, instances.Snowflake, instances.ClientRaindrop)
+  }
+
+  //"Lower-level" convenience function
+  async function addToIdentityRegistry(userIdentity, IdentityRegistryInstance, SnowflakeInstance, ClientRaindropInstance){
+
+    const timestamp = Math.round(new Date() / 1000) - 1
+    const permissionString = web3.utils.soliditySha3(
+      '0x19', '0x00', IdentityRegistryInstance.address,
+      'I authorize the creation of an Identity on my behalf.',
+      userIdentity.recoveryAddress,
+      userIdentity.address,
+      { t: 'address[]', v: [SnowflakeInstance.address] },
+      { t: 'address[]', v: [] },
+      timestamp
+    )
+
+    const permission = await sign(permissionString, userIdentity.address, userIdentity.private)
+
+    await SnowflakeInstance.createIdentityDelegated(
+      userIdentity.recoveryAddress, userIdentity.address, [], userIdentity.hydroID, permission.v, permission.r, permission.s, timestamp
+    )
+    //console.log("EIN:    " + userIdentity.ein)
+    userIdentity.identity = web3.utils.toBN(userIdentity.ein)
+
+    await verifyIdentity(userIdentity.identity, IdentityRegistryInstance, {
+      recoveryAddress:     userIdentity.recoveryAddress,
+      associatedAddresses: [userIdentity.address],
+      providers:           [SnowflakeInstance.address],
+      resolvers:           [ClientRaindropInstance.address]
+   })
+
+  }
 
 
 }
--- test/marketplace/test-data.js
+++ test/marketplace/test-data.js
@@ -43,6 +43,18 @@ const TestData = {
     }
   ],
 
+  'itemTags': [
+    [
+      "crypto",
+      "software",
+      "stickers",
+      "1st edition",
+      "limited edition",
+      "shirts",
+      "backpacks"
+    ]
+  ],
+
   'itemListings': [
     {
       uuid: 7329140802,
@@ -69,6 +81,30 @@ const TestData = {
       delivery: [], deliveryExpected: undefined,
       tags: [], tagsExpected: undefined,
       returnPolicy: 1
+    }
+  ],
+
+  'returnPolicies': [
+    { returnsAccepted: true, timeLimit: 20000 },
+    { returnsAccepted: false, timeLimit: 50 }
+  ],
+
+  'availableCoupons': [
+    { 
+      couponType: enums.CouponType.AMOUNT_OFF,
+      title: '50 HYDRO Test Discount!' ,
+      description: 'A small little discount for you to cherish for a while during its highly transient existence',
+      amountOff: 50,
+      itemsApplicable: [], itemsApplicableExpected: undefined,
+      expirationDate: 1571312124
+    }, 
+    { 
+      couponType: enums.CouponType.PERCENTAGE_OFF,
+      title: '20% OFF Test Discount!' ,
+      description: 'A HUGE LITTLE discount for you to cherish for a while during its highly transient existence',
+      amountOff: 20,
+      itemsApplicable: [1,2,3], itemsApplicableExpected: [1,2,3],
+      expirationDate: 1591312124
     }
   ]
 
