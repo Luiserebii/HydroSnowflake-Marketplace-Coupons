51a3f284626a3bb95867142c78aead4543c74596
Luiserebii <luis@serebii.io>
2019-03-29 18:17:05 -0400

    Setting up marketplace-api.js and test-data.js; refactoring starts here

---
+++ test/marketplace/marketplace-api.js
@@ -0,0 +1,24 @@
+/*
+ * =================== 
+ *   Marketplace API - a general compilation of functions that will ideally ease the writing process. Refactoring will go here
+ * ===================
+ *
+ * 
+ */
+
+const testdata = require('./test-data.js')
+const util = require('util')
+
+const BN = web3.utils.BN
+const allEnums = require('../../enum_mappings/enums.js')
+const enums = allEnums.CouponMarketPlaceResolverInterface;
+
+
+class MarketplaceAPI {
+
+
+
+
+}
+
+module.exports = MarketplaceAPI;
---
+++ test/marketplace/test-data.js
@@ -0,0 +1,26 @@
+/*
+ *
+ * Test Data - simply meant to contain testing inputs for convenience
+ * 
+ *
+ */
+
+
+const allEnums = require('../../enum_mappings/enums.js')
+const enums = allEnums.CouponMarketPlaceResolverInterface;
+
+
+class TestData {
+
+
+
+
+
+
+
+
+
+
+}
+
+module.exports = TestData;
